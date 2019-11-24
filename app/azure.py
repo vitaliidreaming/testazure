01bc933d259da044fec4f947bec65dee07f2d874
Vitalii <>
2019-11-25 00:35:16 +0200

    delete file

--- app/azure.py
+++
@@ -1,4 +0,0 @@
-from app import app
-from azure.storage.blob import BlobServiceClient
-connection_string = app.config.get('AZS')
-service = BlobServiceClient.from_connection_string(conn_str=connection_string)
