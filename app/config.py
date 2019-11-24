e818a36589f38b084e0a39a516d6c1ccf78cacee
Vitalii <>
2019-11-25 00:38:46 +0200

    format

--- app/config.py
+++ app/config.py
@@ -2,6 +2,6 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get("SECRET_KEY") or 'secret'
-    AZS = os.environ.get("AZURE_STORAGE_CONNECTIONSTRING") or 'smstr'
-    AZS_CONTAINER = os.environ.get("AZS_CONTAINER") or 'alise'
+    SECRET_KEY = os.environ.get("SECRET_KEY") or "secret"
+    AZS = os.environ.get("AZURE_STORAGE_CONNECTIONSTRING") or "smstr"
+    AZS_CONTAINER = os.environ.get("AZS_CONTAINER") or "alise"
